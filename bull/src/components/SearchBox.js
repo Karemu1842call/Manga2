d7b86283b484e19fb9540cd363bbd252d18f72ce
Karemu1842call <victorkinoti03@gmail.com>
2022-08-11 15:21:50 +0300

    done

--- bull/src/components/SearchBox.js
+++ bull/src/components/SearchBox.js
@@ -1,26 +1,23 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 function SearchBox(){
      
    const [query,setQuery]=useState('');
-   const [animes, setAnimes] =useState([])
+   const [animes, setAnimes] =useState([]);
+   const [loadanime,setloadanime]=useState(false);
 
-    const searchAnime= async (e)=>{
-        e.preventDefault();
-        const url ='https://my-json-server.typicode.com/Karemu1842call/project-json-server/results';
-    
-        
-        try {
-            const res=await fetch(url);
-            const data=await res.json();
-            console.log(data);
-            setAnimes(data)
-        }catch(err){
-            console.log(err)
+    useEffect(() => {
+        const loadanime=async()=>{
+            setloadanime(true);
+            const response=await axios.get("https://my-json-server.typicode.com/Karemu1842call/project-json-server/results")
+            setAnimes(response.data);
+            setloadanime(false);
         }
-    }
+        loadanime();
+    },[],);
     return(
        <>
-       <form className="form" onSubmit={searchAnime}>
+       <form className="form" onSubmit={loadanime}>
         <label className='label' htmlFor='query'>Anime name</label>
          <div id="search" className="bar">
              <input value={query} name="query" className="tafuta" placeholder='Enter anime name ie Naruto' onInput={(e) => setQuery(e.target.value)} ></input>
@@ -28,13 +25,14 @@ function SearchBox(){
         </div>
        </form>
        <div className='card-list'  >
-        {animes.map(anime =>(
+        {animes.filter((anime) =>anime.name.toLowerCase().includes(query)
+        ).map(anime =>(
             <div className='card' key={anime.id}>
-                <h1>{anime.name}</h1>
-                <h2>{anime.maincharacter}</h2>
+                <h1 key={anime.id}>{anime.name}</h1>
                 <img className='card--image'
                 src={anime.image}
                 alt={anime.name + 'poster'}/>
+                <h1 key={anime.id}>{anime.casts}</h1>
                 <div className="card--content">
                     <h2 className="card--tittle">{anime.maincharacter}</h2>
                     </div>
